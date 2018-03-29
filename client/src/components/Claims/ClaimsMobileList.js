3ac7f1402eb644cf4d26dfcb00f42c564a476ca9
Igor Gorodetskyy <igorgo16@gmail.com>
2018-03-29 07:29:38 +0300

    wip

--- client/src/components/Claims/ClaimsMobileList.js
+++ client/src/components/Claims/ClaimsMobileList.js
@@ -2,7 +2,9 @@ import {QList, QItem, QCard, QCardMain, scroll} from 'quasar'
 
 export default {
   computed: {
-    scrollTarget () { return scroll.getScrollTarget(this.$refs['claims-filter-option'].$el) }
+    scrollTarget () {
+      return scroll.getScrollTarget(this.$refs['claims-filter-option'].$el)
+    }
   },
   methods: {
     _getRows (h) {
@@ -26,31 +28,47 @@ export default {
             }
           },
           [
-            h(
-              QCard,
-              {
-                staticClass: `claim-list-card text-black claim-list-card-t${claim.claimType}`,
-                props: { color: 'grey-1' }
-              },
-              [
-                h(
-                  QCardMain,
-                  {
-                    staticClass: 'q-pa-md'
-                  },
-                  [
-                    claim.unit
-                      ? claim.unit.replace(/;/g, ', ')
-                      : 'Рекламація'
-                  ]
-                )
-              ]
-            )
+            this._getRow(h, claim)
           ]
         )
         rows.push(item)
       })
       return rows
+    },
+    _getRow (h, claim) {
+      return h(
+        QCard,
+        {
+          staticClass: `claim-list-card text-black claim-list-card-t${claim.claimType}`,
+          props: { color: 'grey-1' }
+        },
+        [
+          h(
+            QCardMain,
+            {
+              staticClass: 'q-pa-none'
+            },
+            [
+              h(
+                'div', {
+                  staticClass: 'row justify-start'
+                }, [
+                  h('div', {
+                    staticClass: 'col-sm-11 col-9'
+                  }, [
+                    claim.unit
+                      ? claim.unit.replace(/;/g, ', ')
+                      : 'Рекламація'
+                  ]),
+                  h('div', {
+                    staticClass: 'col-sm-1 col-3'
+                  }, [
+                    h('div', { staticClass: 'float-right claim-row-priority-corner' }),
+                    h('div', {staticClass: 'claim-row-priority text-white'}, [claim.priority])
+                  ])
+                ])
+            ])
+        ])
     },
     _getList (h) {
       return h(QList, { props: { noBorder: true }, ref: 'list' }, this._getRows(h))
